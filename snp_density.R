9253000b075fccc5671b0c181a777012bc715e3d
Rezwan Tariq <42382456+RezwanCAAS@users.noreply.github.com>
2024-02-21 13:50:58 +0300

    Update snp_density.R

--- snp_density.R
+++ snp_density.R
@@ -1,36 +1,11 @@
 
 
-setRepositories()
-library(CMplot)
-library(tidyverse)
-library(data.table)
-
-
-#upload the plink.map file
-map1 <- fread("~/path/genotyped_hapmap.map", header = F)
-
-head(map1, n = 20)
-
-mm <- map1 %>%
-  dplyr::select(SNP = 2, Chromosome = 1, Position = 4)
-
-head(mm)
-
-CMplot(mm, plot.type = "d", bin.size = 1e6, col = c("darkgreen", "yellow", "red"),
-       file = "tiff", dpi = 300, file.output = TRUE, verbose = TRUE)
-
-
-
-
-
-
-
 library(CMplot)
 library(tidyverse)
 library(data.table)
 
 #upload .map file
-map1 <- fread("/Users/tariqr/Desktop/snp_density/genotyped_hapmap.map", header = F)
+map1 <- fread("`/path/genotyped_hapmap.map", header = F)
 
 head(map1)
 
